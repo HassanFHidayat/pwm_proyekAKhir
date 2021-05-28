c357c83ed7a8aedd46c58cc0c3afdea2d67ca467
HassanFHidayat <hassanfhidayat@gmail.com>
2021-05-28 12:56:40 +0700

    upload1

--- js/app.js
+++ js/app.js
@@ -2,13 +2,18 @@ function getElement(id) {
     return document.getElementById(id);
 }
 
+//membuat variabel Date
 var d = new Date
+//mendapatkan tahun sekarang dengan method date
 var y = d.getFullYear();
+//mendapatkan bulan sekarang dengan method date
 var m = d.getMonth();
 
+//mengubah bulan yang berformat (0-11) menjadi nama bulan dalam string
 var bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 getElement("header").innerHTML = "Jadwal Shalat Bulan " + bulan[m] + " " + y;
 
+//mengambil data dari api dan menampilkannya ke file jadwal.html dalam bentuk tabel
 fetch('https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/yogyakarta/' + y + '/' + '0' + (m + 1) + '.json')
     .then(res => res.json())
     .then((res) => {
